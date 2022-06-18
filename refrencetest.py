97e8f2369e5af99e98ba5a1a9b0d63a9c102eada
isha <vshlkmr81@gmail.com>
2022-06-18 12:20:39 +0530

    tuple

--- refrencetest.py
+++ refrencetest.py
@@ -1,3 +1,6 @@
+from this import s
+
+
 a=10
 b=10
 print(id(b))
@@ -5,3 +8,15 @@ a=20
 print(id(a))
 y=a
 print(id(y))
+
+# 
+
+str="hello world"
+print(str)
+print(type(str))
+list =[10,20,30,"vishal"]
+print(type(list))
+print(list[1])
+
+tu =(10,20)
+print(type(tu))
